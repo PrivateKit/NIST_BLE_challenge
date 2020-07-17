49a70a9e20a4be5ad3aa082f5a002c99660cf486
Sheshank Shankar <sheshanks@aithinktank.com>
2020-07-16 21:59:02 -0700

    oh encoding

--- scripts/rnn.py
+++ scripts/rnn.py
@@ -3,7 +3,8 @@ from sklearn.model_selection import train_test_split
 import os
 import random
 import math
-
+import time
+import torch
 import torch.nn as nn
 
 NUM_READINGS_PER_INTERVAL = 150
@@ -11,6 +12,24 @@ INTERVAL_LENGTH = 4  # length in seconds of each interval
 
 
 def load_data(key_path, data_path):
+    # first pass to find the various values for the categorical features to use for one-hot encoding
+    fixed_variables_key = [set() for _ in range(9)]
+    for file_id in os.listdir(data_path):
+        if file_id.startswith("."):
+            # there are some weird extra files starting with .
+            continue
+        with open(os.path.join(data_path, file_id), 'r', errors="ignore") as data_file:
+            for index in range(7):
+                value = data_file.readline().strip().split(",")[1]
+                fixed_variables_key[index].add(value)
+    with open(key_path, 'r', errors="ignore") as key_file:
+        key_file.readline()  # skip header
+        for line in key_file:
+            transmitter_position, receiver_position = line.split("\t")[1].split("_")
+            fixed_variables_key[7].add(transmitter_position)
+            fixed_variables_key[8].add(receiver_position)
+    fixed_variables_key = [list(x) for x in fixed_variables_key]
+
     X = list()
     y = list()
     with open(key_path, 'r') as key_file:
@@ -20,13 +39,13 @@ def load_data(key_path, data_path):
             file_id = record[0]
             file_data = list()
             with open(os.path.join(data_path, file_id)) as data_file:
-                # fixed variables
-                tx_device, tx_power, rx_device, tx_carry, rx_carry, rx_pose, tx_pose = \
-                    [data_file.readline().strip().split(",")[1] for _ in range(7)]
-                transmitter_position, receiver_position = record[1].split("_")
-                # todo: add one hot encoding
-                fixed_part = [tx_device, tx_power, rx_device, tx_carry, rx_carry, rx_pose, tx_pose,
-                                   transmitter_position, receiver_position]
+                # fixed variables tx_device, tx_power, rx_device, tx_carry, rx_carry, rx_pose, tx_pose,
+                #                               transmitter_position, receiver_position
+                fixed_variables = [data_file.readline().strip().split(",")[1] for _ in range(7)]
+                fixed_variables.extend(record[1].split("_"))
+                fixed_part = list()
+                for variable, key in zip(fixed_variables, fixed_variables_key):
+                    fixed_part.extend([int(possible_value == variable) for possible_value in key])
                 if len(record) == 5:
                     # this means this file has labels
                     distance = float(record[2])
@@ -116,3 +135,17 @@ if __name__ == "__main__":
     train_X, val_X, train_y, val_y = train_test_split(dev_X, dev_y)
 
     # todo: model
+    rnn = RNN(150, 128, 1)
+    criterion = nn.NLLLoss()
+
+    # train model
+    n_iters = len(train_y)
+    print_every = 100
+    plot_every = 25
+
+    current_loss = 0
+    all_losses = []
+
+    for i in range(1, n_iters + 1):
+        distance = train_y[i]
+
